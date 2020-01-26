f23e1abdb1c138653175d3e774bb107e2a9719dd
Jiwon <chloe326o.o@gmail.com>
2020-01-26 22:09:04 +0900

    435.py solution changed

--- leetcode/435.py
+++ leetcode/435.py
@@ -8,29 +8,20 @@ def eraseOverlapIntervals(intervals):
         """
 	if intervals == []:
 		return 0
-        intervals = sorted(intervals, key=lambda i:i[0])
+        intervals = sorted(intervals, key=lambda i:i[1])
 	res,cnt,tmp = [],0,0
-	res.append(intervals[0])
-	i = 0 
-	while i < (len(intervals)-1):
-		if res[-1][1] > intervals[i+1][0]:
-			tmp +=1
+	end = float('-inf')
+	for i in range((len(intervals))):
+		if end <= intervals[i][0]:
+			end = intervals[i][1]
+		else :
 			cnt +=1
-		else :
-			res.append(intervals[i+1])
-			tmp = 0
-		if tmp > 1 :
-			cnt -= 1
-			tmp = 0
-			res.append(intervals[i])
-		else :
-			i+=1	
 	return cnt
 
-arr = [[1,10],[2,3],[3,4]]
+arr =[[0,2],[1,3],[1,3],[2,4],[3,5],[3,5],[4,6]]
 print eraseOverlapIntervals(arr)
 
-arr = [[1,2],[1,3],[2,3],[3,4]]
+arr = [[1,100],[11,22],[1,11],[2,12]]
 print eraseOverlapIntervals(arr)
 
 arr = [[1,2],[1,2],[1,2]]
