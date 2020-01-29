f5bb1b7acdfeafe52d032cf5b28746cdeaae2620
Jiwon <chloe326o.o@gmail.com>
2020-01-29 23:27:33 +0900

    Group Anagrams - solution(accepted)

--- GroupAnagrams.py
+++ GroupAnagrams.py
@@ -3,21 +3,7 @@
 # Given an array of strings, group anagrams together.
 import collections
 def groupAnagrams(strs):
-	res, idx = [],[]
-	cnt = 0
-	for i in range(len(strs)):
-		if i in idx:
-			continue
-		res.append([])
-		tmp = collections.Counter(strs[i])
-		for j in range(i, len(strs),1):
-			if j not in idx:
-				tmp2 = collections.Counter(strs[j])
-				if tmp == tmp2:
-					idx.append(j)
-					res[cnt].append(strs[j])
-		cnt+=1	
-	return res	
-
-strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
-print groupAnagrams(strs)
+	ans = collections.defaultdict(list)
+	for s in strs:
+		ans[tuple(sorted(s))].append(s)
+	return ans.values()
