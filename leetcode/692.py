4bc6142bd8fc7bb227bfd25990d9192cbdf49d5f
Jiwon <chloe326o.o@gmail.com>
2020-02-20 23:15:48 +0900

    692.py - on my own

--- leetcode/692.py
+++ leetcode/692.py
@@ -20,3 +20,46 @@ class Solution(object):
         for i in range(k):
             res.append(sortedDic[i][0])
         return res
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Solution 2 - on my own
+# Using Dictionary, sort them with value and alphabetical order if values are the same
+# beat 84.13%
+# Learned to use lambda with sorted function 
+# Time Complexity O(nLogn) : for using sort
+class Solution(object):
+    def topKFrequent(self, words, k):
+        """
+        :type words: List[str]
+        :type k: int
+        :rtype: List[str]
+        """
+	dic = {}
+        for word in words:
+                if word not in dic:
+                        dic[word] = 0
+                else :
+                        dic[word]+=1
+        res = sorted(dic.items(), key=lambda x:(-x[1],x[0]))
+        res = res[0:k]
+        word = []
+        for tup in res:
+                word.append(tup[0])
+        return word
+
