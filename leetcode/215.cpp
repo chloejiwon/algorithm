49670ac8dcbc93d24be3768da7facd51d3cfa99f
Jiwon <chloe326o.o@gmail.com>
2020-03-28 15:07:29 +0900

    FizzBuzz

--- leetcode/215.cpp
+++ leetcode/215.cpp
@@ -1,19 +1,33 @@
+#include <iostream>
+#include <map>
+#include <vector>
+
+using namespace std;
+
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
         if(nums.size() == 0)
             return 0;
         map<int,int> m;
-        for(auto & num : nums){
+        for(int & num : nums){
             m[num]++;
         }
         k = nums.size() - k +1 ; 
-        for(auto &iter :m){
-            k -= iter.second;
+        for(auto iter = m.begin(); iter != m.end(); iter++){
+            k -= iter->second;
             if(k <= 0)
-                return iter.first;
+                return iter->first;
         }
         return 0;
         
     }
 };
+
+int main(){
+    Solution sol;
+    vector<int> nums{1,3,4,2,3,1};
+    int k = 2;
+    cout << sol.findKthLargest(nums, k) << endl;
+    return 0;
+}
