e4b8253a40bcaf8b9d55970312bc21982887c941
Jiwon <chloe326o.o@gmail.com>
2020-04-04 15:33:13 +0900

    Line2019_ solution

--- CodingTest/line2019.cpp
+++ CodingTest/line2019.cpp
@@ -48,12 +48,51 @@ class Solution{
             else
                 return -1;
         }
+        
+    int solve(int conyPosition, int brownPosition) {
+        int time = 0;
+        bool visit[200001][2];
+        queue<pair<int, int> > queue;
+        memset(visit, 0, sizeof(visit));
+        queue.push(make_pair(brownPosition, 0));
+        while (1) {
+        conyPosition += time;
+            if (conyPosition > 200000)
+                return -1;
+            if (visit[conyPosition][time % 2])
+                return time;
+            for (int i = 0, size = queue.size(); i < size; i++) {
+                int currentPosition = queue.front().first;
+                int newTime = (queue.front().second + 1) % 2;
+                int newPosition;
+                queue.pop();
+                newPosition = currentPosition - 1;
+                if (newPosition >= 0 && !visit[newPosition][newTime]) {
+                    visit[newPosition][newTime] = true;
+                    queue.push(make_pair(newPosition, newTime));
+                }
+                newPosition = currentPosition + 1;
+                if (newPosition < 200001 && !visit[newPosition][newTime]) {
+                    visit[newPosition][newTime] = true;
+                    queue.push(make_pair(newPosition, newTime));
+                }
+                newPosition = currentPosition * 2;
+                if (newPosition < 200001 && !visit[newPosition][newTime]) {
+                    visit[newPosition][newTime] = true;
+                    queue.push(make_pair(newPosition, newTime));
+                }
+            }
+            time++;
+        }
+    }
 };
 int main(){
     int C, B;
     //cin >> C >> B ;
-    C = 11; B = 2;
+    C = 1; B = 2;
     Solution sol;
     cout << sol.solution(C,B) << endl;
+
+    //cout << sol.solve(C,B) << endl;
     return 0;
 }
