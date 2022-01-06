9871796e22664b7d4707a948b30d3d3610b2ed21
chloejiwon <chloe326o.o@gmail.com>
2022-01-06 23:28:56 +0900

    mv 56.py

--- leetcode/56.py
+++ leetcode/56.py
@@ -28,3 +28,15 @@ class Solution(object):
             res.append(intervals[-1])
             
         return res
+
+
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        merged = []
+        for i in sorted(intervals, key=lambda x:x[0]):
+            if merged and i[0] <= merged[-1][1]:
+                merged[-1][1] = max(i[1], merged[-1][1])
+            else:
+                merged += [i]
+
+        return merged
